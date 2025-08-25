bbbadb3136ba0ad18a14b43180355425be5fac07
jailsetubal <jail.setubal@outlook.com>
2025-08-25 16:26:30 -0300

    Realização de atividade utilizando a linguagem MarkDown

---
+++ Heranca/src/main/java/org/example/Cavalo.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Cavalo extends Animal {
+    public Cavalo(String nome, String raca, String cor, int idade) {
+        super(nome, raca, cor, idade);
+    }
+
+}
